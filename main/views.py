292ce49d24115d34c1fa5c2306f79a9ab704453b
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-04-23 23:48:07 -0400

    Basic page views

--- main/views.py
+++ main/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def homepage(request):
-    return HttpResponse("Hello There this is <strong>Daves's</strong> Home Page")
+    return HttpResponse("<strong>Hello World</strong> !!! This is <strong>Daves's</strong> Home Page")
+
+def aboutpage(request):
+    return HttpResponse("<strong>Hello World</strong> !!! This is <strong>Daves's</strong> About Page")
+
+def contactpage(request):
+    return HttpResponse("<strong>Hello World</strong> !!! This is <strong>Daves's</strong> Contact Page")
+
+def resumepage(request):
+    return HttpResponse("<strong>Hello World</strong> !!! This is <strong>Daves's</strong> Resume Page")
