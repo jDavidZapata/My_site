307469df748aaa3c93edeed809a3d0687817a939
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-09-30 20:27:13 -0400

    First Test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class MainSiteTestCase(TestCase):
+
+    def test_index(self):
+        c = Client()
+        response = c.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["body"], "Body: HELLO WORLD!!!")
