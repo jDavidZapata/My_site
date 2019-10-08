f271f329d87854f31cc770822543a936971fdbbf
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-10-07 23:45:14 -0400

    Tests

--- main/tests.py
+++ main/tests.py
@@ -1,10 +1,33 @@
 from django.test import TestCase, Client
 
 # Create your tests here.
-class MainSiteTestCase(TestCase):
-
-    def test_index(self):
+class MainPageTest(TestCase):
+    """ Test module for Main Page. """
+    def test_index_page(self):
         c = Client()
         response = c.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["body"], "Body: HELLO WORLD!!!")
+
+
+class ContactPageTest(TestCase):
+
+    def test_contact_page(self):
+        c = Client()
+        response = c.get("/contact/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["title"], "Contact")
+        self.assertEqual(response.context["body"], "Body: How to Contact ME")
+        self.assertEqual(response.context["content"], "732 or 201")
+
+
+class AboutPageTest(TestCase):
+
+    def test_about_page(self):
+        c = Client()
+        response = c.get("/about/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["title"], "About")
+        self.assertEqual(response.context["body"], "Body: About ME!!!")
+        
+
