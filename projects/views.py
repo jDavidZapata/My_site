3aadb55b682b7e90a32208c073f15bdaa49ca307
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-10-14 23:54:12 -0400

    Projects

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from projects.models import Project
+from django.http import Http404
 
 # Create your views here.
 
@@ -13,8 +14,8 @@ def projects_list(request):
 
 
 def project_detail(request, project_id):
-    project = get_object_or_404(Project, pk=project_id)
-    '''try:
+    #project = get_object_or_404(Project, pk=project_id)
+    try:
         project = Project.objects.get(pk=project_id)
         context = {
             'project': project
@@ -25,4 +26,5 @@ def project_detail(request, project_id):
     context = {
             'project': project
         }
+    '''    
     return render(request, 'projects/project_detail.html', context)
