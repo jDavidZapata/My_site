06bfcf19cde10a3fee8c14ae69387bb8f434dd92
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-10-16 22:29:29 -0400

    Projects

--- projects/models.py
+++ projects/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=20)
     goal = models.TextField()
-    image = models.FileField(upload_to='img/')
+    image = models.ImageField(upload_to='img/')
 
     def __str__(self):
         return f"{self.title} -- Build with {self.technology}"
