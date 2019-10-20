bd0c95b2c7a71911921c1e9614c8731764bc2776
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-10-19 23:30:19 -0400

    tests

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class ProjectsListPageTest(TestCase):
+    """ Test module for Projecsts List Page. """
+    def test_projects_page(self):
+        c = Client()
+        response = c.get("/projects/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["title"], "Projects")
+
