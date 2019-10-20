e454d005e0e471669aa3feed27bb45e96edccde9
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-10-19 23:59:28 -0400

    tests

--- projects/tests/test_views.py
+++ projects/tests/test_views.py
@@ -9,3 +9,13 @@ class ProjectsListPageTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["title"], "Projects")
 
+
+class ProjectPageTest(TestCase):
+    """ Test module for Projecst Page. """
+    project_id = 1
+    def test_project_page(self):
+        c = Client()
+        response = c.get("/{project_id}/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["title"], "Project # {project_id}")
+
