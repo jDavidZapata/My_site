34920a3ebe2d6371813dc9169daac9933ac8c9ea
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-10-25 23:53:15 -0400

    Project

--- projects/forms.py
+++ projects/forms.py
@@ -7,6 +7,7 @@ class ProjectForm(forms.Form):
     description = forms.CharField()
     technology = forms.CharField(max_length=20)
     goal = forms.CharField()
+    url = forms.CharField(max_length=200)
     image = forms.ImageField()
 
 
@@ -14,4 +15,4 @@ class ProjectModelForm(forms.ModelForm):
 
     class Meta:
         model = Project
-        fields = ['title', 'description', 'technology', 'goal', 'image']
+        fields = ['title', 'description', 'technology', 'goal', 'url', 'image']
--- projects/models.py
+++ projects/models.py
@@ -8,7 +8,14 @@ class Project(models.Model):
     technology = models.CharField(max_length=20)
     goal = models.TextField()
     image = models.ImageField(upload_to='img/')
+    url =  models.URLField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return f"{self.title} -- Build with {self.technology}"
     
+
+    def __repr__(self):
+        return f"{self.title} -- Build with {self.technology}"
