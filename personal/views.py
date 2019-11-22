a2c23b2ce4ffeca51c56edb540dd5b0a7077313c
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-11-21 23:46:31 -0500

    personal

--- personal/views.py
+++ personal/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def temppage(request):
+
+    personal = True
+    template_name = 'personal/temp.html'
+    context = {
+        "title": "Temporary",
+        "body": "Body: Temp Page",
+        "personal0": "True"
+    }
+    return render(request, template_name, context)
