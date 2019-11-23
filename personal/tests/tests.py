091f04746ae8199a911b0bdba578d767edd70e5f
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-11-22 23:51:05 -0500

    personal test

--- personal/tests/tests.py
+++ personal/tests/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+class PersonalPageTest(TestCase):
+    """ Test module for Personal Page. """
+    def test_personal_page(self):
+        c = Client()
+        response = c.get("/personal/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["title"], "Temporary")
+        self.assertEqual(response.context["body"], "Body: Temp Page")
