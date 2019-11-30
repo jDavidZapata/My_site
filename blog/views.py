575d81773c54818f594e2f00baba4a7118713bf8
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-11-29 22:48:08 -0500

    blog post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, get_list_or_404
+from .models import Post
 
 # Create your views here.
+
+def posts_list(request):
+
+    template_name = 'blog/posts_list.html'
+    posts = get_list_or_404(Post)
+    context = {
+            'title': '* Posts *',
+            'posts': posts
+        }
+
+    return render(request, template_name, context)
+
+
+def post_detail(request, post_id):
+
+    template_name = 'blog/post_detail.html'
+    post = get_object_or_404(Post, pk=post_id)
+   
+    context = {
+            'title': f'Post # {post_id}',
+            'post': post
+        }        
+    return render(request, template_name, context)
