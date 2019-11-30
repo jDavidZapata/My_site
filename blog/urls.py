d8acc613a24513ea9daba47856a6c721a130b458
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-11-30 13:40:19 -0500

    posts

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path("", views.posts_list, name="posts_list"),
+    path("<int:post_id>/", views.post_detail, name="post_detail"),
+    #path("create/", views.post_create, name="post_create"),
+    #path("<int:post_id>/update/", views.post_update, name="post_update"),
+    #path("<int:post_id>/delete/", views.post_delete, name="post_delete"),
+]
