aedc6d0ab94c7c8618409b37fe37420ce6be4da5
Juan David Zapata <jdavidzapata1983@gmail.com>
2020-02-12 22:28:14 -0500

    test users

--- users/tests/test_selenium.py
+++ users/tests/test_selenium.py
@@ -8,7 +8,7 @@ USER2 = 'oteruser@example.com'
 PASSWORD = 'pAs$w0rd!!'
 EMAIL = 'user@example.com'
 
-
+### Set up Data base with fixtures
 def create_user(username=USER, password=PASSWORD, email=EMAIL): 
     return get_user_model().objects.create_user(
         username=username, password=password, email=email)
@@ -29,6 +29,22 @@ class UsersSeleniumTests(StaticLiveServerTestCase):
         cls.selenium.quit()
         super().tearDownClass()
 
+    def test_signup(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/register/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys(USER)
+        password_input = self.selenium.find_element_by_name("password1")
+        password_input.send_keys(PASSWORD)
+        password_input = self.selenium.find_element_by_name("password2")
+        password_input.send_keys(PASSWORD)
+        password_input = self.selenium.find_element_by_name("email")
+        password_input.send_keys(EMAIL)
+        #self.selenium.find_element_by_xpath('//input[@value="login"]').click()
+        #self.selenium.find_element_by_tag_name("button").click()
+        self.selenium.find_element_by_id("signup").click()
+
+
+
     def test_login(self):
         self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
         username_input = self.selenium.find_element_by_name("username")
@@ -38,3 +54,17 @@ class UsersSeleniumTests(StaticLiveServerTestCase):
         #self.selenium.find_element_by_xpath('//input[@value="login"]').click()
         #self.selenium.find_element_by_tag_name("button").click()
         self.selenium.find_element_by_id("login").click()
+
+    '''
+    def test_logout(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys(USER)
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys(PASSWORD)
+        #self.selenium.find_element_by_xpath('//input[@value="login"]').click()
+        #self.selenium.find_element_by_tag_name("button").click()
+        self.selenium.find_element_by_id("login").click()
+        self.selenium.implicitly_wait(10)
+        self.selenium.find_element_by_id("logout").click()
+    '''
