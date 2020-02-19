b23cbb521b34d40cf5a514f15400060c036ac1c8
Juan David Zapata <jdavidzapata1983@gmail.com>
2020-02-19 00:24:45 -0500

    tests

--- blog/test/test_selenium.py
+++ blog/test/test_selenium.py
@@ -125,3 +125,32 @@ class BlogSeleniumTests(StaticLiveServerTestCase):
         content_input = self.selenium.find_element_by_name("content")
         content_input.send_keys(CONTENT)
         self.selenium.find_element_by_class_name("btn").click()
+
+
+
+    
+    def test_comment_create(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/register/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys(USER)
+        password1_input = self.selenium.find_element_by_name("password1")
+        password1_input.send_keys(PASSWORD)
+        password2_input = self.selenium.find_element_by_name("password2")
+        password2_input.send_keys(PASSWORD)
+        email_input = self.selenium.find_element_by_name("email")
+        email_input.send_keys(EMAIL)
+        self.selenium.find_element_by_id("signup").click()
+        self.selenium.get('%s%s' % (self.live_server_url, '/personal/blog/create-category/'))
+        name_input = self.selenium.find_element_by_name("name")
+        name_input.send_keys(NAME)
+        summary_input = self.selenium.find_element_by_name("summary")
+        summary_input.send_keys(SUMMARY)
+        self.selenium.find_element_by_class_name("btn").click()
+        self.selenium.get('%s%s' % (self.live_server_url, '/personal/blog-post/'))
+        title_input = self.selenium.find_element_by_name("title")
+        title_input.send_keys(TITLE)
+        content_input = self.selenium.find_element_by_name("content")
+        content_input.send_keys(CONTENT)
+        self.selenium.find_element_by_class_name("btn").click()
+        self.selenium.get('%s%s' % (self.live_server_url, '/personal/blog/category/first-post'))
+        
